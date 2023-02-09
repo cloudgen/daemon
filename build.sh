f1c14c40dde6d46708a4caabc28eff873d6a99f9
Cloudgen Wong <cloudgen.wong@gmail.com>
2023-02-09 15:08:17 +0800

    Update project with control by unix socket

--- build.sh
+++ build.sh
@@ -1,18 +1,21 @@
 #!/bin/sh
-
+mkdir -p target
 gcc src/main.c \
-    src/singleton.c \
-    src/run-detached.c \
     src/log-message.c \
     src/make-directory.c \
-    src/main-loop.c \
+    src/receiver.c \
+    src/receiver-loop.c \
     src/remove-old-log.c \
-    -o daemon
+    src/run-detached.c \
+    src/send-cmd.c \
+    src/singleton.c \
+    -o target/daemon
 
 if [ $? -ne 0 ]; then
     echo "Build failed. Please check the error messages above."
     exit 1
 else
     echo "Build successful!"
+    echo "  You can run the program by: \"target/daemon\""
     exit 0
 fi
--- src/main.c
+++ src/main.c
@@ -3,12 +3,14 @@
 #include "singleton.h"
 #include "daemon-info.h"
 #include "log-message.h"
-#include "main-loop.h"
+#include "receiver.h"
 #include "make-directory.h"
 #include "remove-old-log.h"
 #include "run-detached.h"
+#include "send-cmd.h"
 
 void handle_exit(int sig) {
+  unlink(SOCKET_NAME);
   unlink(PID_FILE);
   log_message("Terminated");
   exit(0);
@@ -16,20 +18,23 @@ void handle_exit(int sig) {
 
 int main(int argc, char *argv[]) {
 
-  check_singleton();
   if (make_directory(LOG_DIR) != 0) {
     fprintf(stderr, "Error creating directory\n");
     return 1;
   }
 
   if (argc > 1 && strcmp(argv[1], "-d") == 0) {
-    printf("Daemon mode\n");
+    check_singleton();
+    printf("Starting Daemon mode\n");
     run_detached();
+    signal(SIGINT, handle_exit);
+    signal(SIGTERM, handle_exit);
+    log_message("Daemon Started");
+    create_pid_file();
+    receiver(); 
+    log_message("Daemon Ended Unexpected");
+  } else {
+    send_cmd(argv[1]);
   }
-  create_pid_file();
-  signal(SIGINT, handle_exit);
-  signal(SIGTERM, handle_exit);
-  log_message("Started");
-  main_loop();  
   return 0;
 }
